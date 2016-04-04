5c348e845285a27dc143dc71c22afa80bc02feb2
Arkadiusz Kondas <arkadiusz.kondas@gmail.com>
2016-04-04 22:48:07 +0200

    add more Euclidean distance tests

--- tests/Phpml/Metric/DistanceTest.php
+++ tests/Phpml/Metric/DistanceTest.php
@@ -10,9 +10,42 @@ class DistanceTest extends \PHPUnit_Framework_TestCase
     /**
      * @expectedException \Phpml\Exception\InvalidArgumentException
      */
-    public function testThrowExceptionOnInvalidArguments()
+    public function testThrowExceptionOnInvalidArgumentsInEuclidean()
     {
-        Distance::euclidean([0, 1, 2], [0, 2]);
+        $a = [0, 1, 2];
+        $b = [0, 2];
+
+        Distance::euclidean($a, $b);
+    }
+
+    public function testCalculateEuclideanDistanceForOneDimension()
+    {
+        $a = [4];
+        $b = [2];
+
+        $expectedDistance = 2;
+        $actualDistance = Distance::euclidean($a, $b);
+
+        \PHPUnit_Framework_Assert::assertEquals($expectedDistance, $actualDistance);
+    }
+
+    public function testCalculateEuclideanDistanceForTwoAndMoreDimension()
+    {
+        $a = [4, 6];
+        $b = [2, 5];
+
+        $expectedDistance = 2.2360679774998;
+        $actualDistance = Distance::euclidean($a, $b);
+
+        \PHPUnit_Framework_Assert::assertEquals($expectedDistance, $actualDistance);
+
+        $a = [6, 10, 3];
+        $b = [2, 5, 5];
+
+        $expectedDistance = 6.7082039324993694;
+        $actualDistance = Distance::euclidean($a, $b);
+
+        \PHPUnit_Framework_Assert::assertEquals($expectedDistance, $actualDistance);
     }
 
 }
