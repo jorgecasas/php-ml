f6a561158654ea8689fd3155f586ccb76106ab27
Arkadiusz Kondas <arkadiusz.kondas@gmail.com>
2016-04-04 21:50:30 +0200

    move functions definitions

--- src/Phpml/Classifier/NaiveBayes.php
+++ src/Phpml/Classifier/NaiveBayes.php
@@ -5,22 +5,6 @@ namespace Phpml\Classifier;
 abstract class NaiveBayes implements Classifier
 {
 
-    /**
-     * @param array $features
-     * @param array $labels
-     */
-    public function train($features, $labels)
-    {
 
-    }
-
-    /**
-     * @param mixed $feature
-     * @return mixed
-     */
-    public function predict($feature)
-    {
-
-    }
 
 }
