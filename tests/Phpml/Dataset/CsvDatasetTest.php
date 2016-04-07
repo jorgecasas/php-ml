db9d57cd221e182aa086d69598b91ffbe9873072
Arkadiusz Kondas <arkadiusz.kondas@gmail.com>
2016-04-07 22:36:02 +0200

    add tests for datasets

---
+++ tests/Phpml/Dataset/CsvDatasetTest.php
@@ -0,0 +1,28 @@
+<?php
+
+declare (strict_types = 1);
+
+namespace tests\Phpml\Dataset;
+
+use Phpml\Dataset\CsvDataset;
+
+class CsvDatasetTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * @expectedException \Phpml\Exception\DatasetException
+     */
+    public function testThrowExceptionOnMissingFile()
+    {
+        new CsvDataset('missingFile', 3);
+    }
+
+    public function testSampleCsvDataset()
+    {
+        $filePath = dirname(__FILE__).'/Resources/dataset.csv';
+
+        $dataset = new CsvDataset($filePath, 2, true);
+
+        $this->assertEquals(10, count($dataset->getSamples()));
+        $this->assertEquals(10, count($dataset->getLabels()));
+    }
+}
