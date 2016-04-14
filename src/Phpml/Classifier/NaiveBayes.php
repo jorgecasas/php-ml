100205d76778aa008da9b6ce0ebc1be56355800a
Arkadiusz Kondas <arkadiusz.kondas@gmail.com>
2016-04-14 22:56:54 +0200

    simple Naive Bayes classifier

--- src/Phpml/Classifier/NaiveBayes.php
+++ src/Phpml/Classifier/NaiveBayes.php
@@ -7,11 +7,23 @@ namespace Phpml\Classifier;
 class NaiveBayes implements Classifier
 {
     /**
+     * @var array
+     */
+    private $samples;
+
+    /**
+     * @var array
+     */
+    private $labels;
+
+    /**
      * @param array $samples
      * @param array $labels
      */
     public function train(array $samples, array $labels)
     {
+        $this->samples = $samples;
+        $this->labels = $labels;
     }
 
     /**
@@ -21,5 +33,38 @@ class NaiveBayes implements Classifier
      */
     public function predict(array $samples)
     {
+        if (!is_array($samples[0])) {
+            $predicted = $this->predictSample($samples);
+        } else {
+            $predicted = [];
+            foreach ($samples as $index => $sample) {
+                $predicted[$index] = $this->predictSample($sample);
+            }
+        }
+
+        return $predicted;
+    }
+
+    /**
+     * @param array $sample
+     *
+     * @return mixed
+     */
+    private function predictSample(array $sample)
+    {
+        $predictions = [];
+        foreach ($this->labels as $index => $label) {
+            $predictions[$label] = 0;
+            foreach ($sample as $token => $count) {
+                if (array_key_exists($token, $this->samples[$index])) {
+                    $predictions[$label] += $count * $this->samples[$index][$token];
+                }
+            }
+        }
+
+        arsort($predictions, SORT_NUMERIC);
+        reset($predictions);
+
+        return key($predictions);
     }
 }
