5170c1077325f5c0b929f2934d9955de37bd716a
Arkadiusz Kondas <arkadiusz.kondas@gmail.com>
2016-04-16 21:34:50 +0200

    simplify csv dataset

--- src/Phpml/Dataset/CsvDataset.php
+++ src/Phpml/Dataset/CsvDataset.php
@@ -29,16 +29,15 @@ class CsvDataset extends ArrayDataset
         if(false === $handle = fopen($filepath, 'r')) {
             throw DatasetException::cantOpenFile(basename($filepath));
         }
-        $row = 0;
+
+        if ($headingRow) {
+            fgets($handle);
+        }
+
         while (($data = fgetcsv($handle, 1000, ',')) !== false) {
-            ++$row;
-            if ($headingRow && $row == 1) {
-                continue;
-            }
             $this->samples[] = array_slice($data, 0, $features);
             $this->labels[] = $data[$features];
         }
         fclose($handle);
-
     }
 }
