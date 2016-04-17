ded28def2d1c4531c8ff81d07fd83973511b9332
Arkadiusz Kondas <arkadiusz.kondas@gmail.com>
2016-04-17 17:47:30 +0200

    fix minkowski distance

--- src/Phpml/Metric/Distance/Minkowski.php
+++ src/Phpml/Metric/Distance/Minkowski.php
@@ -36,7 +36,7 @@ class Minkowski implements Distance
             throw InvalidArgumentException::sizeNotMatch();
         }
 
-        $distance = 1;
+        $distance = 0;
         $count = count($a);
 
         for ($i = 0; $i < $count; ++$i) {
