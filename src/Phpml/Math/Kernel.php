37782eba98ea62f245b5b6a6fd4674191b884263
Arkadiusz Kondas <arkadiusz.kondas@gmail.com>
2016-04-21 22:54:38 +0200

    implement RBF kernel function

---
+++ src/Phpml/Math/Kernel.php
@@ -0,0 +1,17 @@
+<?php
+declare(strict_types = 1);
+
+namespace Phpml\Math;
+
+interface Kernel
+{
+
+    /**
+     * @param float $a
+     * @param float $b
+     *
+     * @return float
+     */
+    public function compute($a, $b);
+
+}
