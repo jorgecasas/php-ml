b5e4cbe66e8b4e50353f2fe5a98c97ad2d2098ce
Arkadiusz Kondas <arkadiusz.kondas@gmail.com>
2016-04-27 23:57:23 +0200

    add Mean::arithmetic tests

---
+++ tests/Phpml/Math/Statistic/MeanTest.php
@@ -0,0 +1,17 @@
+<?php
+
+declare (strict_types = 1);
+
+namespace test\Phpml\Math\StandardDeviation;
+
+use Phpml\Math\Statistic\Mean;
+
+class MeanTest extends \PHPUnit_Framework_TestCase
+{
+    public function testArithmeticMean()
+    {
+        $delta = 0.01;
+        $this->assertEquals(3.5, Mean::arithmetic([2, 5]), '', $delta);
+        $this->assertEquals(41.16, Mean::arithmetic([43, 21, 25, 42, 57, 59]), '', $delta);
+    }
+}
