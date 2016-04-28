3e4dc3ddf8c1db9e3f89bcdcde052c4a0309c7f0
Arkadiusz Kondas <arkadiusz.kondas@gmail.com>
2016-04-28 07:32:48 +0200

    add test for mean with floats

--- tests/Phpml/Math/Statistic/MeanTest.php
+++ tests/Phpml/Math/Statistic/MeanTest.php
@@ -13,5 +13,6 @@ class MeanTest extends \PHPUnit_Framework_TestCase
         $delta = 0.01;
         $this->assertEquals(3.5, Mean::arithmetic([2, 5]), '', $delta);
         $this->assertEquals(41.16, Mean::arithmetic([43, 21, 25, 42, 57, 59]), '', $delta);
+        $this->assertEquals(1.7, Mean::arithmetic([0.5, 0.5, 1.5, 2.5, 3.5]), '', $delta);
     }
 }
