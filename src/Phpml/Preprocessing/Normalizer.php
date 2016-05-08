fb04b578535ad17f303cc508bde0e771e2c194ea
Arkadiusz Kondas <arkadiusz.kondas@gmail.com>
2016-05-08 20:35:01 +0200

    implement data Normalizer with L1 and L2 norm

---
+++ src/Phpml/Preprocessing/Normalizer.php
@@ -0,0 +1,83 @@
+<?php
+
+declare (strict_types = 1);
+
+namespace Phpml\Preprocessing;
+
+use Phpml\Exception\NormalizerException;
+
+class Normalizer implements Preprocessor
+{
+    const NORM_L1 = 1;
+    const NORM_L2 = 2;
+
+    /**
+     * @var int
+     */
+    private $norm;
+
+    /**
+     * @param int $norm
+     *
+     * @throws NormalizerException
+     */
+    public function __construct(int $norm = self::NORM_L2)
+    {
+        if (!in_array($norm, [self::NORM_L1, self::NORM_L2])) {
+            throw NormalizerException::unknownNorm();
+        }
+
+        $this->norm = $norm;
+    }
+
+    /**
+     * @param array $samples
+     */
+    public function preprocess(array &$samples)
+    {
+        $method = sprintf('normalizeL%s', $this->norm);
+        foreach ($samples as &$sample) {
+            $this->$method($sample);
+        }
+    }
+
+    /**
+     * @param array $sample
+     */
+    private function normalizeL1(array &$sample)
+    {
+        $norm1 = 0;
+        foreach ($sample as $feature) {
+            $norm1 += abs($feature);
+        }
+
+        if (0 == $norm1) {
+            $count = count($sample);
+            $sample = array_fill(0, $count, 1.0 / $count);
+        } else {
+            foreach ($sample as &$feature) {
+                $feature = $feature / $norm1;
+            }
+        }
+    }
+
+    /**
+     * @param array $sample
+     */
+    private function normalizeL2(array &$sample)
+    {
+        $norm2 = 0;
+        foreach ($sample as $feature) {
+            $norm2 += $feature * $feature;
+        }
+        $norm2 = sqrt($norm2);
+
+        if (0 == $norm2) {
+            $sample = array_fill(0, count($sample), 1);
+        } else {
+            foreach ($sample as &$feature) {
+                $feature = $feature / $norm2;
+            }
+        }
+    }
+}
