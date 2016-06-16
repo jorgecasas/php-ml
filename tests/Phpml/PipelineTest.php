15519ba12201cf3eca6a2c8e151c895a248262d6
Arkadiusz Kondas <arkadiusz.kondas@gmail.com>
2016-06-16 09:58:17 +0200

    simple pipeline test

---
+++ tests/Phpml/PipelineTest.php
@@ -0,0 +1,27 @@
+<?php
+
+declare (strict_types = 1);
+
+namespace tests;
+
+use Phpml\Classification\SVC;
+use Phpml\FeatureExtraction\TfIdfTransformer;
+use Phpml\Pipeline;
+
+class PipelineTest extends \PHPUnit_Framework_TestCase
+{
+
+    public function testPipelineConstruction()
+    {
+        $transformers = [
+            new TfIdfTransformer()
+        ];
+        $estimator = new SVC();
+
+        $pipeline = new Pipeline($transformers, $estimator);
+
+        $this->assertEquals($transformers, $pipeline->getTransformers());
+        $this->assertEquals($estimator, $pipeline->getEstimator());
+    }
+
+}
